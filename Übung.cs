b89fe26c41545cfce7bd877a8ce22876d6f6a767
01raijoh <01raijoh@rgtfo-me.it>
2018-03-06 11:19:52 +0100

    Programm

---
+++ Übung.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Übung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo");
+        }
+    }
+}
